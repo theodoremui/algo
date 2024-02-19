db246c0affd27b0e4837e067a0781bf7eee8d518
theodoremui <theodoremui@gmail.com>
2024-02-18 22:17:45 -0800

    adding contest folder

---
+++ contest/feb24/Test.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        
+        pw.println("Hello world");
+        pw.close();
+    }
+}
